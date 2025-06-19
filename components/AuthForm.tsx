28ed409324fee8a60e6993130cae26402968c3aa
Sanivj <himsanivj18@gmail.com>
2025-06-19 15:48:18 +0530

    Initial project setup and authentication UI completed
    - Set up Next.js project structure
    - Integrated Tailwind CSS for styling
    - Built and styled the authentication form (sign in/sign up)
    - Added form validation with React Hook Form and Zod
    - Created reusable UI components
    - Project ready for further development

---
+++ components/AuthForm.tsx
@@ -0,0 +1,66 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+})
+import React from 'react'
+import Image from "next/image"
+
+function AuthForm({type}:{type:FormType}) {
+    // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
+  const isSignIn=type==='sign-in';
+  return (
+    // This wrapper centers the form vertically and horizontally
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="card-border lg:min-w-[556px] items-center justify-center flex flex-col gap-6">
+        <div className="flex flex-col gap-6 card py-14 px-10">
+          <div className="flex flex-row gap-2 justify-center">
+            <Image src="/logo.svg" alt="logo" height={32} width={38}/>
+            <h2 className="text-primary-100">PrepWise</h2>
+          </div>
+          <h3>Practice Job interviews with the help of AI</h3>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
+            {!isSignIn&&<p>Name</p>}
+            <p>Email</p>
+            <p>Password</p>
+            <Button className="btn" type="submit">{isSignIn?'Sign-in':'Create an Account'}</Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  )
+}
+
+export default AuthForm
