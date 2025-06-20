823209ce55f00b9f3056bce0506f4fe57ba4d050
Sanivj <himsanivj18@gmail.com>
2025-06-20 10:42:36 +0530

    updated client.ts
    - removed API keys from client.ts and added them to .env.local
    - referenced them to client.ts

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC_Pi65pMghc4nWLiAtKp1Ej33IPA9KHCM",
-  authDomain: "prepwise-f74ba.firebaseapp.com",
-  projectId: "prepwise-f74ba",
-  storageBucket: "prepwise-f74ba.appspot.com", // fixed typo: should be .appspot.com
-  messagingSenderId: "204033341703",
-  appId: "1:204033341703:web:442ccd3f515a62fee56da2",
-  measurementId: "G-4ZVNNLDNEX"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET, 
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
